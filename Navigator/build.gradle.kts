2cc1dd89a10c7d7386e3b34d7d462af6ed7f9477
galaniberico <alexgalanp@gmail.com>
2024-03-23 23:02:25 +0100

    Changed namespace

--- Navigator/build.gradle.kts
+++ Navigator/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    `maven-publish`
 }
 
 android {
-    namespace = "com.example.navigator"
+    namespace = "software.galaniberico.navigator"
     compileSdk = 34
 
     defaultConfig {
@@ -24,16 +25,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
+    implementation(libs.moduledroid)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
